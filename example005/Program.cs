d0e587fba6c97e84e187d47276ee0382fb4590a9
serg <123456st@mail.ru>
2023-04-06 17:19:27 +0800

    ввод Маха

---
+++ example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello,имя пользователя");
+string user = Console.ReadLine();
+if (user.ToLower() == "маша")
+{
+    Console.WriteLine("привет Маха!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(user);
+
+
+
+}
